32877915dd619856f0274aa711cd09533d1078af
S. Veronese <90195017+SJVeronese@users.noreply.github.com>
2022-06-24 10:22:55 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="nicci",
+    version="0.9.1",
+    url="",
+    author="Simone Veronese",
+    author_email="veronese@astron.nl",
+    description="A collection of functions for the analysis of astronomical data cubes and images",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=['astropy','numpy','matplotlib','pandas','pvextractor','scipy'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
